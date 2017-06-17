5225c850379181dd6a25f3eb024e86f0cb1c4161
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-17 15:55:29 -0700

    ruby fb chatbot application.rb

--- Chatbot/config/application.rb
+++ Chatbot/config/application.rb
@@ -11,5 +11,9 @@ module Chatbot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Auto-load /bot and its subdirectories
+    config.paths.add File.join("app", "bot"), glob: File.join("**","*.rb")
+    config.autoload_paths += Dir[Rails.root.join("app", "bot", "*")]
   end
 end
