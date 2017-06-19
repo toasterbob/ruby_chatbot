27792a253969bc99bea8fca639e6402e47813ba8
Mark Noizumi <mark.noizumi@gmail.com>
2017-06-18 22:21:54 -0700

    gem edits

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Chatbot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #config.web_console.whiny_requests = false
+    config.web_console.whiny_requests = false
     # Auto-load /bot and its subdirectories
     config.paths.add File.join("app", "bot"), glob: File.join("**","*.rb")
     config.autoload_paths += Dir[Rails.root.join("app", "bot", "*")]
